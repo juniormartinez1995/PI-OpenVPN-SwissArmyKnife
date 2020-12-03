7872bb69e0fededb414eb7161a65361d0f387d58
juniormartinez1995 <engmartinez00@gmail.com>
2020-12-02 21:48:26 -0300

    Finalização de regras iptables e instalação

--- OpenVPNInstaller.sh
+++ OpenVPNInstaller.sh
@@ -39,7 +39,7 @@ dialog                                       \
      --title 'Perfil'                          \
      --menu 'Escolha o que deseja fazer na instalação:'  \
      0 0 0                                     \
-     Instalar_VPN       'Instala e configura o serviço VPN'                    \
+     Remover programa       'Desinstala o serviço OpenVPN'                    \
      Adicionar_Cliente    'Adiciona clientes a VPN já existente'               \
      Deletar_Cliente  'Deleta o cliente e suas credenciais do servidor'
 
@@ -93,24 +93,24 @@ set_dns () {
 
 }
 
-add_new_cliente () {
+new_client () {
         {
-                cat /etc/openvpn/server/cliente-common.txt
-                echo "<ca>"
-                cat /etc/openvpn/server/easy-rsa/pki/ca.crt
-                echo "</ca>"
-                echo "<cert>"
-                sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$cliente".crt
-                echo "</cert>"
-                echo "<key>"
-                cat /etc/openvpn/server/easy-rsa/pki/private/"$client".key
-                echo "</key>"
-                echo "tls-crypt>"
-                sed -ne '/BEGIN OpenVPN Static Key/,$ p' /etc/openvpn/server/tc.key
-                echo "</tls-crypt>"
-        } > ~/"client".ovpn
-
+        cat /etc/openvpn/server/client-common.txt
+        echo "<ca>"
+        cat /etc/openvpn/server/easy-rsa/pki/ca.crt
+        echo "</ca>"
+        echo "<cert>"
+        sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt
+        echo "</cert>"
+        echo "<key>"
+        cat /etc/openvpn/server/easy-rsa/pki/private/"$client".key
+        echo "</key>"
+        echo "<tls-crypt>"
+        sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
+        echo "</tls-crypt>"
+        } > ~/"$client".ovpn
 }
+
 
 if [[ ! -e /etc/openvpn/server/server.conf ]]; then
         clear
@@ -119,13 +119,14 @@ if [[ ! -e /etc/openvpn/server/server.conf ]]; then
         if echo "$ip" | grep -qE '^(10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.|192\.168)'; then
                 echo 'to aqui'
                 public_ip=$(curl -s https://api.ipify.org)
+                #echo $public_ip
+                #ip=$public_ip
         fi
 
 
         if [[ $(ip -6 addr | grep -c 'inet6 [23]') -eq 1 ]]; then
                 ip6=$(ip -6 addr | grep 'inet6 [23]' | cut -d '/' -f 1 | grep -oE '([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}')
         fi
-        #echo $public_ip
 
         choice=$( dialog --stdout --menu 'Protocolo' 0 0 0 1 UDP 2 TCP)
 
@@ -153,6 +154,12 @@ if [[ ! -e /etc/openvpn/server/server.conf ]]; then
 
         client=$( dialog --stdout --inputbox 'Digite o nome do cliente:' 0 0 )
 
+        if [ -z $client ]; then
+                client="client"
+        fi
+        echo $client
+        sleep 10
+
         dialog --yesno 'Deseja começar a instalação?' 0 0
 
 
@@ -173,14 +180,137 @@ if [[ ! -e /etc/openvpn/server/server.conf ]]; then
 
 
         mkdir -p /etc/openvpn/server/easy-rsa/
-        { wget -q0- 'https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz' 2>/devl/null || curl -sL 'https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz' ; } | tar xz -C /etc/openvpn/server/easy-rsa/ --strip-components 1
-        chow -R root:root /etc/openvpn/server/easy-rsa/
+        { wget -q0- 'https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz' 2>/dev/null || curl -sL 'https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz' ; } | tar xz -C /etc/openvpn/server/easy-rsa/ --strip-components 1
+        chown -R root:root /etc/openvpn/server/easy-rsa/
         cd /etc/openvpn/server/easy-rsa/
 
         ./easyrsa init-pki
-	./easyrsa --batch build-ca nopass
-	EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-server-full server nopass
-	EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full "$client" nopass
-	EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
+        ./easyrsa --batch build-ca nopass
+        EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-server-full server nopass
+        EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full "$client" nopass
+        EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
+
+
+        cp pki/ca.crt pki/private/ca.key pki/issued/server.crt pki/private/server.key pki/crl.pem /etc/openvpn/server
+        chown nobody:"$group_name" /etc/openvpn/server/crl.pem
+        chmod o+x /etc/openvpn/server/
+        openvpn --genkey --secret /etc/openvpn/server/tc.key
+
+        echo '-----BEGIN DH PARAMETERS-----
+MIIBCAKCAQEA//////////+t+FRYortKmq/cViAnPTzx2LnFg84tNpWp4TZBFGQz
++8yTnc4kmz75fS/jY2MMddj2gbICrsRhetPfHtXV/WVhJDP1H18GbtCFY2VVPe0a
+87VXE15/V8k1mE8McODmi3fipona8+/och3xWKE2rec1MKzKT0g6eXq8CrGCsyT7
+YdEIqUuyyOP7uWrat2DX9GgdT0Kj3jlN9K5W7edjcrsZCwenyO4KbXCeAvzhzffi
+7MA0BM0oNC9hkXL+nOmFg/+OTxIy7vKBg8P+OxtMb61zO7X8vC7CIAXFjvGDfRaD
+ssbzSibBsu/6iGtCOGEoXJf//////////wIBAg==
+-----END DH PARAMETERS-----' > /etc/openvpn/server/dh.pem
+
+        #Arquivo de config do servidor
+        echo "local $ip
+port $port
+proto $protocol
+dev tun
+ca ca.crt
+cert server.crt
+key server.key
+dh dh.pem
+auth SHA512
+tls-crypt tc.key
+topology subnet
+server 10.8.0.0 255.255.255.0" > /etc/openvpn/server/server.conf
+
+        if [[ -z "$ip6" ]]; then
+                echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server/server.conf
+        else
+                echo 'server-ipv6 fddd:1194:1194:1194::/64' >> /etc/openvpn/server/server.conf
+                echo 'push "redirect-gateway def1 ipv6 bypass-dhcp"' >> /etc/openvpn/server/server.conf
+        fi
+
+        echo 'ifconfig-pool-persist ipp.txt' >> /etc/openvpn/server/server.conf
+
+        set_dns $dns
+
+        echo "keepalive 10 120
+cipher AES-256-CBC
+user nobody
+group $group_name
+persist-key
+persist-tun
+status openvpn-status.log
+verb 3
+crl-verify crl.pem" >> /etc/openvpn/server/server.conf
+
+        set_protocol $protocol
+
+        echo 'net.ipv4.ip_forward=1' > /etc/sysctl.d/30-openvpn-forward.conf
+        echo 1 > /proc/sys/net/ipv4/ip_forward
+        if [[ -n "$ip6" ]]; then
+                # Habilitando o net.ipv6.conf.all.forwarding para o sistema
+                echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.d/30-openvpn-forward.conf
+                echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
+        fi
+
+        #Serviço para as regras de iptables
+        iptables_path=$(command -v iptables)
+        ip6tables_path=$(command -v ip6tables)
+
+
+        if [[ $(systemd-detect-virt) == "openvz" ]] && readlink -f "$(command -v iptables)" | grep -q "nft" && hash iptables-legacy 2>/dev/null; then
+                iptables_path=$(command -v iptables-legacy)
+                ip6tables_path=$(command -v ip6tables-legacy)
+        fi
+        echo "[Unit]
+Before=network.target
+[Service]
+Type=oneshot
+ExecStart=$iptables_path -t nat -A POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to $ip
+ExecStart=$iptables_path -I INPUT -p $protocol --dport $port -j ACCEPT
+ExecStart=$iptables_path -I FORWARD -s 10.8.0.0/24 -j ACCEPT
+ExecStart=$iptables_path -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+ExecStop=$iptables_path -t nat -D POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to $ip
+ExecStop=$iptables_path -D INPUT -p $protocol --dport $port -j ACCEPT
+ExecStop=$iptables_path -D FORWARD -s 10.8.0.0/24 -j ACCEPT
+ExecStop=$iptables_path -D FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT" > /etc/systemd/system/openvpn-iptables.service
+                if [[ -n "$ip6" ]]; then
+                        echo "ExecStart=$ip6tables_path -t nat -A POSTROUTING -s fddd:1194:1194:1194::/64 ! -d fddd:1194:1194:1194::/64 -j SNAT --to $ip6
+ExecStart=$ip6tables_path -I FORWARD -s fddd:1194:1194:1194::/64 -j ACCEPT
+ExecStart=$ip6tables_path -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+ExecStop=$ip6tables_path -t nat -D POSTROUTING -s fddd:1194:1194:1194::/64 ! -d fddd:1194:1194:1194::/64 -j SNAT --to $ip6
+ExecStop=$ip6tables_path -D FORWARD -s fddd:1194:1194:1194::/64 -j ACCEPT
+ExecStop=$ip6tables_path -D FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT" >> /etc/systemd/system/openvpn-iptables.service
+                fi
+                echo "RemainAfterExit=yes
+[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/openvpn-iptables.service
+                systemctl enable --now openvpn-iptables.service
+
+
+        [[ -n "$public_ip" ]] && ip="$public_ip"
+
+        echo "client
+dev tun
+proto $protocol
+remote $ip $port
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+remote-cert-tls server
+auth SHA512
+cipher AES-256-CBC
+ignore-unknown-option block-outside-dns
+block-outside-dns
+verb 3" > /etc/openvpn/server/client-common.txt
+
+
+        systemctl enable --now openvpn-server@server.service
+
+        new_client
+
+else
+        echo "Tratar para quando o VPN ja tiver sido instalado"
+
 
 fi
+
+
